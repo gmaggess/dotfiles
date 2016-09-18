63609b9519493f8893bc28313420b03525c9ed32
gmaggess <george.maggessy@gmail.com>
2016-09-18 10:55:50 -0700

    Adding fish scripts and removing some stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,13 @@
-set default_user "paulirish"
-set default_machine "paulirish-macbookair2"
+set default_user "gmaggessy"
+set default_machine "gmaggessy-mbp"
 
 
-#set -x  DYLD_FALLBACK_LIBRARY_PATH /Users/paulirish/.homebrew/lib
+#set -x  DYLD_FALLBACK_LIBRARY_PATH /Users/gmaggessy/.homebrew/lib
 
-source ~/.config/fish/path.fish
+# source ~/.config/fish/path.fish
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/chpwd.fish
 source ~/.config/fish/functions.fish
-
-
-
-
 
 # Completions
 function make_completion --argument-names alias command
@@ -27,6 +23,33 @@ end
 
 make_completion g 'git'
 
+# Font
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+# set -g theme_display_git no
+# set -g theme_display_git_untracked no
+# set -g theme_display_git_ahead_verbose yes
+# set -g theme_git_worktree_support yes
+# set -g theme_display_vagrant yes
+# set -g theme_display_docker_machine no
+# set -g theme_display_hg yes
+# set -g theme_display_virtualenv no
+# set -g theme_display_ruby no
+# set -g theme_display_user yes
+# set -g theme_display_vi yes
+# set -g theme_display_vi_hide_mode default
+# set -g theme_display_date no
+# set -g theme_display_cmd_duration yes
+# set -g theme_title_display_process yes
+# set -g theme_title_display_path no
+# set -g theme_title_use_abbreviated_path no
+# set -g theme_date_format "+%a %H:%M"
+# set -g theme_avoid_ambiguous_glyphs yes
+# set -g theme_powerline_fonts no
+# set -g theme_nerd_fonts yes
+# set -g theme_show_exit_status yes
+# set -g default_user your_normal_user
+# set -g theme_color_scheme dark
 
 # Readline colors
 set -g fish_color_autosuggestion 555 yellow
@@ -53,24 +76,29 @@ set -g fish_color_dimmed 555
 set -g fish_color_separator 999
 
 # Git prompt status
-set -g __fish_git_prompt_showdirtystate 'yes'
-set -g __fish_git_prompt_showupstream auto
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
 
+set -g __fish_git_prompt_color_branch magenta --bold
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
 
-# Status Chars
-#set __fish_git_prompt_char_dirtystate '*'
-set __fish_git_prompt_char_upstream_equal ''
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_dirtystate 'red'
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
 
-set __fish_git_prompt_color_upstream_ahead ffb90f
-set __fish_git_prompt_color_upstream_behind blue
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green --bold
 
 # Local prompt customization
 set -e fish_greeting
-
 
 set -g fish_pager_color_completion normal
 set -g fish_pager_color_description 555 yellow
