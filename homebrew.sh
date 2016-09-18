954e5baa26922c61790122f8bf39a33794bf1073
gmaggess <george.maggessy@gmail.com>
2016-09-18 01:50:08 -0700

    Adding more file

---
+++ homebrew.sh
@@ -0,0 +1,13 @@
+##############################################################################################################
+### homebrew!
+
+# (if your machine has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
+mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
+export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
+
+# install all the things
+./brew.sh
+./brew-cask.sh
+
+### end of homebrew
+##############################################################################################################
