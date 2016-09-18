fdd470a639a1d0d2aaafefffa6011a80d1c9c076
gmaggess <george.maggessy@gmail.com>
2016-09-18 01:36:47 -0700

    Adding some goodies from Paul Irish dotfiles

---
+++ fish/path.fish
@@ -0,0 +1,22 @@
+
+# Grab my $PATHs from ~/.extra
+set -l PATH_DIRS (cat "$HOME/.extra" | grep "^PATH" | \
+    # clean up bash PATH setting pattern
+    sed "s/PATH=//" | sed "s/\\\$PATH://" | \
+    # rewrite ~/ to use {$HOME}
+    sed "s/~\//{\$HOME}\//")
+
+
+set -l PA ""
+
+for entry in (string split \n $PATH_DIRS)
+    # resolve the {$HOME} substitutions
+    set -l resolved_path (eval echo $entry)
+    if test -d "$resolved_path"; # and not contains $resolved_path $PATH
+        set PA $PA "$resolved_path"
+    end
+end
+
+set PA $PA /Users/paulirish/.rvm/gems/ruby-2.2.1/bin
+
+set --export PATH $PA
