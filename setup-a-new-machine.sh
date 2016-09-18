216e42a695756b0449a86e2b5b1406396894f8cc
gmaggess <george.maggessy@gmail.com>
2016-09-18 01:50:08 -0700

    Adding more file

---
+++ setup-a-new-machine.sh
@@ -0,0 +1,57 @@
+# copy paste this file in bit by bit.
+# don't run it.
+  echo "do not run this script in one go. hit ctrl-c NOW"
+  read -n 1
+
+##############################################################################################################
+###  backup old machine's key items
+./migration.sh
+
+##############################################################################################################
+### XCode Command Line Tools
+#      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
+./xcode.sh
+
+##############################################################################################################
+### homebrew!
+./homebrew.sh
+
+##############################################################################################################
+### install of common things
+###
+./common.sh
+
+##############################################################################################################
+### remaining configuration
+###
+
+# go read mathias, paulmillr, gf3, alraa's dotfiles to see what's worth stealing.
+
+# prezto and antigen communties also have great stuff
+#   github.com/sorin-ionescu/prezto/blob/master/modules/utility/init.zsh
+
+# set up osx defaults
+#   maybe something else in here https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
+sh .osx
+
+# setup and run Rescuetime!
+
+###
+##############################################################################################################
+
+
+
+##############################################################################################################
+### symlinks to link dotfiles into ~/
+###
+
+#   move git credentials into ~/.gitconfig.local    	http://stackoverflow.com/a/13615531/89484
+#   now .gitconfig can be shared across all machines and only the .local changes
+
+# symlink it up!
+./symlink-setup.sh
+
+# add manual symlink for .ssh/config and probably .config/fish
+
+###
+##############################################################################################################
