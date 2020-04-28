248dac53b7620dd36cd6e6acbc3fef020ce971eb
George Maggessy <george.maggessy@oracle.com>
2020-04-28 08:39:28 -0500

    Consolidate ORACLE and PERSONAL mode

---
+++ bin/GateKeeper_Helper.command
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Bash Menu Script Example
+RED='\033[0;31m'
+GRN='\033[0;32m'
+BLU='\033[0;34m'
+NC='\033[0m'
+echo ""
+echo -e "GateKeeper Helper Brought to you by ${RED}NMac.to${NC}"
+echo ""
+echo -e "${RED}Options 1: Disable Your GateKeeper means${NC}"
+echo -e "${RED}You can completely disable your gatekeeper >> Good For Professional User.${NC}"
+echo ""
+echo -e "${RED}Options 2: Enable Your GateKeeper means${NC}"
+echo -e "${RED}You can completely enable your gatekeeper >> Good For Newbie User.${NC}"
+echo ""
+echo -e "${RED}Options 3: Allow Single App To ByPass The GateKeeper means${NC}"
+echo -e "${RED}If you don't want to completely disable your gatekeeper then allow an${NC}"
+echo -e "${RED}individual app to pass it >> Recommended For All Users.${NC}"
+echo ""
+echo -e "${RED}Options 4: Self-Sign An App means${NC}"
+echo -e "${RED}If you don't want to disable SIP and your app is quite unexpectedly especially${NC}"
+echo -e "${RED}under the recent macOS then try to Self-Sign your app using this option.${NC}"
+echo ""
+PS3='Please enter your choice: '
+options=("Disable Your GateKeeper" "Enable Your GateKeeper" "Allow Single App To ByPass The GateKeeper" "Self-Sign An App" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+		"Disable Your GateKeeper")
+			echo ""
+			echo -e "${GRN}You Choose To Disable Your GateKeeper${NC}"
+			echo ""
+			echo -e "${RED}Plaese Inseret Your Password To Procceed${NC}"
+			echo ""
+			sudo spctl --master-disable
+			break
+			;;
+		"Enable Your GateKeeper")
+			echo ""
+			echo -e "${GRN}You Choose To Enable Your GateKeeper${NC}"
+			echo ""
+			echo -e "${RED}Plaese Inseret Your Password To Procceed${NC}"
+			echo ""
+			sudo spctl --master-enable
+			break
+			;;
+		"Allow Single App To ByPass The GateKeeper")
+			echo ""
+			echo -e "${GRN}You Choose To Allow Single App To ByPass The GateKeeper${NC}"
+			echo ""
+			read -e -p "Drag & Drop The App Here Then Hit Return: " FILEPATH
+			echo ""
+			echo -e "${RED}Plaese Inseret Your Password To Procceed${NC}"
+			echo ""
+			sudo xattr -rd com.apple.quarantine "$FILEPATH"
+			break
+			;;
+		"Self-Sign An App")
+			echo ""
+			echo -e "${GRN}You Choose To Self-Sign An App${NC}"
+			echo ""
+			read -e -p "Drag & Drop The App Here Then Hit Return: " FILEPATH
+			echo ""
+			echo -e "${RED}Plaese Inseret Your Password To Procceed${NC}"
+			echo ""
+			sudo codesign -f -s - --deep "$FILEPATH"
+			echo ""
+			echo -e "${RED}If you see - replacing existing signature - that means you are DONE!${NC}"
+			echo ""
+			echo -e "${RED}Otherwise there is something wrong with the app or its path${NC}"
+			echo ""
+			break
+			;;
+		"Quit")
+			break
+			;;
+		*) echo "invalid option $REPLY";;
+	esac
+done
--- brew-cask.sh
+++ brew-cask.sh
@@ -9,74 +9,69 @@
 brew tap homebrew/cask
 brew update
 
-# daily
 # brew cask install 1password
-brew cask install alfred
-brew cask install dash
-brew cask install dropbox
-brew cask install flux
-# brew cask install google-drive
-# brew cask install google-photos-backup
-# brew cask install onedrive
-# brew cask install keep
-# brew cask install spectacle
-brew cask install proxifier
-
-# dev
-brew cask install vnc-viewer
-brew cask install balsamiq-mockups
-brew cask install docker
+# brew cask install anaconda
+# brew cask install android-studio
+# brew cask install arrsync
+# brew cask install chromium
 # brew cask install docker-toolbox
-brew cask install intellij-idea-ce
-brew cask install iterm2
-brew cask install sourcetree
-brew cask install atom
-brew cask install sublime-text
-brew cask install sublime-merge
+brew cask install firefox-developer-edition
+brew cask install firefox-esr
+brew cask install ganttproject
+brew cask install google-chrome-canary
+brew cask install hyper
+brew cask install skitch
+# brew cask install keep
+# brew cask install limechat
+# brew cask install onedrive
+# brew cask install opera
+# brew cask install skype
+# brew cask install smcfancontrol
+# brew cask install spectacle
 # brew cask install vagrant-manager
 # brew cask install virtualbox
 # brew cask install virtualbox-extension-pack
-brew cask install visual-studio-code
-# brew cask install webstorm
-
-# fun
-# brew cask install limechat
-# brew cask install skype
-
-#browsers
-brew cask install google-chrome
-# brew cask install google-chrome-canary
-# brew cask install chromium
-# brew cask install firefoxdeveloperedition
-# brew cask install firefoxnightly
-# brew cask install webkit-nightly
-# brew cask install opera
-
-# less often
-brew cask install appcleaner
-# brew cask install arrsync
-brew cask install bartender
-brew cask install caffeine
-brew cask install disk-inventory-x
-brew cask install gimp
-brew cask install keka
-brew cask install kindle
-# brew cask install smcfancontrol
-brew cask install the-unarchiver
-brew cask install transmission
 # brew cask install vlc
-brew cask install whatsapp
-# brew cask install android-studio
-# brew cask install anaconda
-
-brew tap homebrew/cask-versions
-brew cask install firefox-esr
-
+# brew cask install webkit-nightly
+# brew cask install webstorm
+# dev
+# fun
+# less often
+#browsers
 #font
-brew tap homebrew/cask-fonts
-brew cask install font-source-code-pro
-brew cask install font-source-code-pro-for-powerline
-brew cask install font-sourcecodepro-nerd-font-mono
+brew cask install alfred
+brew cask install appcleaner
+brew cask install atom
+brew cask install busycal
+brew cask install caffeine
+brew cask install dash
+brew cask install disk-inventory-x
+brew cask install disk-inventory-x
+brew cask install docker
+brew cask install dropbox
+brew cask install firefox-esr
+brew cask install flux
 brew cask install font-fira-code
 brew cask install font-firacode-nerd-font
 brew cask install font-firacode-nerd-font-mono
+brew cask install font-source-code-pro
+brew cask install font-source-code-pro-for-powerline
+brew cask install font-sourcecodepro-nerd-font-mono
+brew cask install gimp
+brew cask install gitter
+brew cask install google-chrome
+brew cask install intellij-idea-ce
+brew cask install iterm2-nightly
+brew cask install keka
+brew cask install kindle
+brew cask install proxifier
+brew cask install sourcetree
+brew cask install sublime-merge
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install transmission
+brew cask install visual-studio-code
+brew cask install vnc-viewer
+brew cask install whatsapp
+brew tap homebrew/cask-fonts
+brew tap homebrew/cask-versions
