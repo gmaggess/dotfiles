fbe7a72435c1676dfc6c1468d8578cd8e768ffec
George Maggessy <george.maggessy@oracle.com>
2021-11-13 09:24:23 -0600

    new vim, shortcuts, and cleanup

--- bin/envorcl.sh
+++ bin/envorcl.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-export HTTP_PROXY=http://www-proxy.us.oracle.com:80
-export HTTPS_PROXY=http://www-proxy.us.oracle.com:80
-export FTP_PROXY=http://www-proxy.us.oracle.com:80
+export HTTP_PROXY=http://www-proxy-hqdc.us.oracle.com:80
+export HTTPS_PROXY=$HTTP_PROXY
+export FTP_PROXY=$HTTP_PROXY
+export ALL_PROXY=$HTTP_PROXY
 export NO_PROXY=localhost,127.0.0.0/8,*.local,.us.oracle.com,.oraclecorp.com
+export http_proxy=$HTTP_PROXY
+export https_proxy=$HTTP_PROXY
+export ftp_proxy=$HTTP_PROXY
+export no_proxy=$NO_PROXY
 
 npm config set registry https://artifactory-slc.oraclecorp.com/artifactory/api/npm/npmjs-remote
 npm config set email george.maggessy@oracle.com
